4da1b7f632e41527c375815f8874090ac8b9bd75
neo <chojs990222@gmail.com>
2023-11-12 14:01:05 +0900

    Sharing prisma client

--- conduit/src/main.rs
+++ conduit/src/main.rs
@@ -1,10 +1,15 @@
+use std::sync::Arc;
+
 use anyhow::Context;
+use axum::Extension;
+use conduit_domain::prisma::PrismaClient;
 use conduit_router::AppRouter;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    // build our application with a route
-    let router = AppRouter::new();
+    let prisma_client = Arc::new(PrismaClient::_builder().build().await?);
+
+    let router = AppRouter::new().layer(Extension(prisma_client));
 
     axum::Server::bind(&format!("0.0.0.0:{}", 3000).parse().unwrap())
         .serve(router.into_make_service())
--- domain/src/lib.rs
+++ domain/src/lib.rs
@@ -1,1 +1,2 @@
-
+pub mod prisma;
+pub mod users;
