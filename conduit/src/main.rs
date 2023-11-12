a446a42752db24c7798b488c9ab0660359e3bcad
neo <chojs990222@gmail.com>
2023-11-12 14:30:24 +0900

    Enable cors

--- conduit/src/main.rs
+++ conduit/src/main.rs
@@ -4,12 +4,15 @@ use anyhow::Context;
 use axum::Extension;
 use conduit_domain::prisma::PrismaClient;
 use conduit_router::AppRouter;
+use tower_http::cors::{Any, CorsLayer};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let prisma_client = Arc::new(PrismaClient::_builder().build().await?);
 
-    let router = AppRouter::new().layer(Extension(prisma_client));
+    let cors = CorsLayer::new().allow_methods(Any).allow_origin(Any);
+
+    let router = AppRouter::new().layer(Extension(prisma_client)).layer(cors);
 
     axum::Server::bind(&format!("0.0.0.0:{}", 3000).parse().unwrap())
         .serve(router.into_make_service())
